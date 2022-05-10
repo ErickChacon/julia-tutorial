c54c7cfa71130e4f0deb9a6d518e0eb9cad4fcc9
Erick Chacon <eral.th07@gmail.com>
2022-05-10 10:31:13 -0500

    makefile

--- Makefile.jl
+++ Makefile.jl
@@ -5,9 +5,10 @@ jls = filter(x -> occursin(r"/[0-9]+-.*\.jl$", x), readdir("scripts", join = tru
 ipynbs = replace.(replace.(jls, "scripts" => "notebooks"), ".jl" => ".ipynb")
 
 # Create notebooks
-rm("notebooks", recursive = true)
+rm("notebooks", recursive = true, force = true)
 Literate.notebook.(jls, "notebooks", execute = true)
 
 # Create markdown files
+rm(joinpath("docs", "src"), recursive = true, force = true)
 Literate.markdown.(jls, joinpath("docs", "src"), execute = true)
 
