9bb130d5137891ec17ba9055e357b32f80839227
Erick <eral.th07@gmail.com>
2023-11-19 17:07:18 +0300

    update scripts

--- scripts/20-dataframes.jl
+++ scripts/20-dataframes.jl
@@ -1,41 +1,106 @@
 # # DataFrames
 
+# Julia has a general representation for `Tables` with different implementations. The most
+# used package is [DataFrames.jl](https://dataframes.juliadata.org/). It takes advantage
+# of Julia syntax to define certain operation.
+
+# ## Define a DataFrame
+
 using DataFrames
 
-data = DataFrame(id = 1:100, x = rand(100), y = randn(100))
+n = 100
+data = DataFrame(id = 1:n, x = rand(n), y = randn(n))
 
-# ## subsetting
+# ## Subsetting
+
+# Let's select a column without doing a copy:
 
 data.id
 #-
-data.x
+data[!, :id]
+
+# Modifications on these columns will be reflected in the original data.
+
+xaux = data.x
+#-
+xaux[1] = 100
+#-
+first(data, 2)
+
+# A copy of a column is created as folows:
+
+xaux = data[:, :x]
+#-
+xaux[1] = 10
+#-
+first(data, 2)
+
+# More specific subsetting can be done with:
 
 #-
 data.id[1:10]
 #-
 data.id[90:end]
-
 #-
 data[1:10, 1:2]
 
-# ## transform
+# You can also create a view that references to you DataFrame:
+
+subdata = view(data, 1:10, 1:3)
+subdata.x[1] = rand()
+first(data, 2)
+
+# ## Transform variables
+
+# To perform operations over columns, we use the `Pair` syntax:
+
+transform(data, :x => (z -> z .^ 2))
+#-
+typeof(:x => (z -> z .^ 2))
+
+# We can explicitly provide the output name:
 
 transform(data, :x => (z -> z .^ 2) => :x2)
 
-transform(data, :x => ByRow(sqrt) => :x2)
+# We can also vectorize any function with `ByRow`:
 
-#-
+transform(data, :x => ByRow(sqrt))
+
+# Notice that the previous operations did not modify the original dataset. You can modify
+# your original dataset using the in-place function `transform!`:
 
 transform!(data, :x => (z -> z .^ 2) => :x2)
-
 #-
+data
 
-select(data, :x, :x2)
+# The function `select` works in a similar way, but only includes the desired variables:
 
+select(data, :x => (z -> z .^ 2) => :x2)
 #-
+select(data, :x, :y)
+#-
+typeof(r"^x")
+#-
+select(data, r"^x")
+
+# Let's add new columns:
 
 insertcols!(data, :z => rand(100))
-
 #-
+first(data, 5)
 
-rename(data, :z => :znew)
+# Let's remove columns:
+
+select!(data, Not(:x2))
+#-
+first(data, 5)
+
+# Another simple operation is to rename columns:
+
+rename(data, :x => :xnew, :z => :znew)
+#-
+rename(data, [:x, :z] .=> [:xnew, :znew])
+#-
+rename(var -> var * "_new", data)
+
+# Remember to use `rename!` to actually make the changes on the original dataset.
--- scripts/30-makie.jl
+++ scripts/30-makie.jl
@@ -1,73 +1,150 @@
-
 # # Visualize with Makie
+#
+# In Julia, there are several packages for plotting. One of the prefered options is
+# [Makie.jl](https://docs.makie.org) which is mainly written in Julia. There is also
+# high-level packages written over Makie.jl to make coding easier:
+# [AlgebraOfGraphics.jl](https://aog.makie.org/) and
+# [TidierPlots.jl](https://github.com/TidierOrg/TidierPlots.jl).
 
-# ## Basic Makie
+# ## Packages and data
 
 using DataFrames
-import CairoMakie as MK
+using CairoMakie
 using AlgebraOfGraphics
 
-df = DataFrame(id = 1:100, x = rand(100), y = rand(100))
+df = DataFrame(id = 1:100, x = randn(100), y = randn(100))
 first(df, 5)
 
-#-
+# ## Makie.jl
 
-fig = MK.lines(df.x, df.y)
-MK.scatter!(df.x, df.y, color = :red)
+# Let's start with a `lines` graph:
+
+fig = lines(df.x, df.y)
+
+# You can add a geometry over this current graph:
+
+scatter!(df.x, df.y, color = :red)
 fig
-MK.save("makie.pdf", fig) #src
+save("makie.pdf", fig) #src
 
-#-
+# To customize the graph we need to understand we have the following the componentes are:
+#
+# - Figure: Is the main container with custom attributes like `backgroundcolor`.
+# - Axis: Is the object where a geometry can be added and it is contained in a Figure.
 
-fig = MK.Figure(backgroundcolor = :tomato)
-MK.Axis(fig[1,1], title = "First figure")
-MK.scatter!(df.x, df.y, color = :red)
-MK.Axis(fig[1,2], title = "Second figure")
-MK.lines!(df.x, df.y, color = :blue)
-MK.save("makie.pdf", fig) #src
+fig = Figure(backgroundcolor = :tomato)
+Axis(fig[1,1], title = "First figure")
+scatter!(df.x, df.y, color = :red)
+Axis(fig[1,2], title = "Second figure")
+lines!(df.x, df.y, color = :blue)
+Axis(fig[2,1:2], title = "Third figure")
+scatter!(df.x, df.y, color = :red)
+lines!(df.x, df.y, color = :blue)
+fig
+save("makie.pdf", fig) #src
 
-#-
+# Common arguments for the `Axis` elements are `title`, `xlabel`, `ylabel`.
 
-fig = MK.scatter(df.x, df.y, color = :red, label = "group 1")
-MK.scatter!(rand(100), rand(100), color = :blue, label = "group 2")
-MK.axislegend()
-MK.save("makie.pdf", fig) #src
+# Now let's create a figure with legends:
 
-#-
+fig = scatter(df.x, df.y, color = :red, label = "Normal(0,1)")
+scatter!(rand(100), rand(100), color = :blue, label = "Uniform(0,1)")
+axislegend()
+fig
+save("makie.pdf", fig) #src
+
+# A figure with colorbar:
 
 X = rand(100, 80)
+fig, ax, hm = heatmap(X, colormap = Reverse(:RdBu))
+Colorbar(fig[1,2], hm)
+fig
+save("makie.pdf", fig) #src
 
-fig, ax, hm = MK.heatmap(X, colormap = MK.Reverse(:RdBu))
-MK.Colorbar(fig[1,2], hm)
-MK.save("makie.pdf", fig) #src
+# You can pass the `Figure` and `Axis` attributes to the plotting function:
+
+hist(df.x, color = (:orange, 0.7), strokewidth = 1,
+    axis = (title = "Histogram", xlabel = "x", ylabel = "π(x)"),
+    figure = (backgroundcolor = (:green, 0.6),)
+)
+save("makie.pdf", fig) #src
+
+# Check [Makie.jl](https://docs.makie.org/stable/reference/plots/) to find all available
+# plots.
 
 # ## Algebra of Graphics
 
+# In practice, `Makie.jl` is used for developers and custom plots for publication. We can
+# use `AlgebraOfGraphics.jl` for a high level syntax.
+
 df.group = rand(string.(1:4), 100)
+first(df, 5)
 
-#-
+# Let's see a basic graph using `AlgebraOfGraphics.jl`.
 
-g = data(df) * mapping(:x, :y) * visual(MK.Scatter)
-fig = draw(g)
-MK.save("makie.pdf", fig) #src
+g = data(df) * mapping(:x, :y)
+draw(g)
+save("makie.pdf", draw(g)) #src
 
-#-
+# You can use `DataFrames.jl` syntax to transform variables:
 
-g = data(df) * mapping(:x, :y) * visual(MK.Lines, color = :red)
-fig = draw(g)
-MK.save("makie.pdf", fig) #src
+g = data(df) * mapping(:x => (z -> 10z) => "First variable", :y => "Second variable")
+draw(g)
+save("makie.pdf", draw(g)) #src
 
 
-#-
+# We can use another geometry with `visual`:
+
+g = data(df) * mapping(:x, :y) * visual(Lines, color = :red, linewidth = 3)
+draw(g)
+save("makie.pdf", draw(g)) #src
+
+# Notice that the order does not matter:
+
+g = mapping(:x, :y) * data(df) * visual(Lines, color = :red, linewidth = 3)
+draw(g)
+save("makie.pdf", draw(g)) #src
+
+# We can use the `+` operator to combine graphs:
+
+g = data(df) * mapping(:x, :y, color = :group) * visual(Lines) +
+    data(df) * mapping(:x, :y) * visual(Scatter)
+draw(g)
+save("makie.pdf", draw(g)) #src
+
+# Why is it called `AlgebraOfGraphics.jl`?
 
 g = data(df) * mapping(:x, :y, color = :group) *
-    (visual(MK.Scatter) + visual(MK.Lines))
+    (visual(Lines) + visual(Scatter))
+draw(g)
+save("makie.pdf", draw(g)) #src
+
+# You can factorize any part:
+addnoise(x) = x + rand()
+g = data(df) *
+    (mapping(:x, :y, color = :group) + mapping(:x, :y => addnoise))
+draw(g)
+save("makie.pdf", draw(g)) #src
+
+# Let's separating the plots by group:
+
+g = data(df) * mapping(:x, :y, layout = :group, color = :group) *
+    (visual(Scatter) + visual(Lines))
 fig = draw(g)
-MK.save("makie.pdf", fig) #src
+save("makie.pdf", fig) #src
 
 #-
 
-g = data(df) * mapping(:x, :y, layout = :group, color = :group) *
-    (visual(MK.Scatter) + visual(MK.Lines))
-fig = draw(g)
-MK.save("makie.pdf", fig) #src
+g = data(df) * mapping(:x, :y, row = :group, color = :group) *
+    (visual(Scatter) + visual(Lines))
+fig = draw(g, axis = (width = 130, height = 130))
+save("makie.pdf", fig) #src
+
+#-
+
+g = data(df) * mapping(:x, :y, col = :group, color = :group) *
+    (visual(Scatter) + visual(Lines))
+fig = draw(g, axis = (width = 130, height = 130))
+save("makie.pdf", fig) #src
+
+
