3b3e5d9c29ecd52fce00e770c9ab42e34936bd0a
Erick <eral.th07@gmail.com>
2023-11-19 11:29:53 +0300

    arrays

--- scripts/08-arrays.jl
+++ scripts/08-arrays.jl
@@ -1,4 +1,8 @@
 # # Arrays
+#
+# An array is a multi-dimensional collection of objects. The elements of arrays do not
+# need to be numbers or even from the same type. However, we are interested in numeric
+# arrays.
 
 using LinearAlgebra
 
@@ -8,15 +12,17 @@ using LinearAlgebra
 
 x = [1, 3, 4]
 
-#-
+# Julia allows to perform operations that are almost globally accepted on vectors. For
+# example, let's get the transpose:
 
 x'
 
-#-
+# Or multiply the vector by an scalar:
 
 4x
 
-#-
+# In cases where the operator is not clear, we need to use the dot operator to make
+# element-by-element computations:
 
 1 .+ x
 
@@ -24,9 +30,10 @@ x'
 
 sqrt.(x)
 
-#-
+# The dot operator is atuomatically available for any function:
 
 g(x) = 3 + 2x^2
+#-
 g.(x)
 
 # ### Matrices
@@ -65,6 +72,10 @@ ones(5, 3)
 #-
 
 rand(5, 3)
+
+#-
+
+randn(5, 3)
 
 #-
 
